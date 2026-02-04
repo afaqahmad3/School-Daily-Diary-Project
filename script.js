8743364d96e3f3012e931ca17feba26ba44057db
afaqahmad3 <167011935+afaqahmad3@users.noreply.github.com>
2026-02-04 23:55:22 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+function generateDiary() {
+  const today = new Date();
+
+  date.innerText = today.toLocaleDateString();
+  day.innerText = today.toLocaleString("en-us", { weekday: "long" });
+
+  classOut.innerText = classInput.value;
+
+  eng.innerText = engInput.value;
+  math.innerText = mathInput.value;
+  sci.innerText = sciInput.value;
+  gk.innerText = gkInput.value;
+  sst.innerText = sstInput.value;
+  urdu.innerText = urduInput.value;
+  isl.innerText = islInput.value;
+  notes.innerText = notesInput.value;
+}
+
+
+/* SIGNATURE PREVIEW */
+signInput.onchange = e => {
+  const reader = new FileReader();
+  reader.onload = () => signPreview.src = reader.result;
+  reader.readAsDataURL(e.target.files[0]);
+};
+
+/* DOWNLOAD IMAGE */
+function downloadDiary() {
+  html2canvas(document.getElementById("diary")).then(canvas => {
+    const link = document.createElement("a");
+    link.download = "daily-homework-diary.png";
+    link.href = canvas.toDataURL("image/png");
+    link.click();
+  });
+}
